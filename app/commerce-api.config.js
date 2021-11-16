eaf5aafbce7839cdae89ad7127cfb48eaa4c13ea
Shane Peterson <shane.peterson@xcentium.com>
2021-11-16 16:39:32 -0500

    setting for sandbox

--- app/commerce-api.config.js
+++ app/commerce-api.config.js
@@ -1,9 +1,9 @@
 export const commerceAPIConfig = {
     proxyPath: `/mobify/proxy/api`,
     parameters: {
-        clientId: '71408db0-3500-4221-9870-55725b3a4d85',
-        organizationId: 'f_ecom_zzhc_003',
-        shortCode: 'kv7kzm78',
-        siteId: 'RefArch'
+    clientId: '71408db0-3500-4221-9870-55725b3a4d85',
+    organizationId: 'f_ecom_zzhc_003',
+    shortCode: 'kv7kzm78',
+    siteId: 'RefArch'
     }
 }
